ec1df3d646538002a601a653d09a84771a638099
Christian J. Ramos Ortega <92696797+christian12305@users.noreply.github.com>
2023-04-14 20:19:16 -0400

    Merge Array challenge
    Returns a sorted ascending array resulting from other 2 arrays. Uses pointers to keep track of each array.

---
+++ MergeArray.java
@@ -0,0 +1,39 @@
+package codenights;
+
+import java.util.Arrays;
+
+public class MergeArray {
+
+	public static void main(String[] args) {
+		 int nums1[] = {0}, m = 0, nums2[] = {1}, n = 1;
+		 
+		 merge(nums1, m, nums2, n);
+		 
+		 System.out.println(Arrays.toString(nums1));
+	}
+	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+		
+		//Pointers
+		int i = m-1;
+		int j = n-1;
+		int k = nums1.length-1;
+		
+		while(j >= 0) {
+			
+			if(i >= 0 && nums1[i] > nums2[j]) {
+				nums1[k] = nums1[i];
+				i--;
+				k--;
+			}
+			
+			else {
+				nums1[k] = nums2[j];
+				j--;
+				k--;
+			}
+				
+			
+		}
+
+	}
+}
