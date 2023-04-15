5a426a3c0b34842251acdeb349c007167279defd
Christian J. Ramos Ortega <92696797+christian12305@users.noreply.github.com>
2023-04-14 20:05:31 -0400

    Palindrome challenge
    Type: string manipulation.

---
+++ Palindrome.java
@@ -0,0 +1,37 @@
+package codenights;
+
+import java.util.*;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int x = scan.nextInt();
+		
+		boolean result = isPalindrome(x);
+		
+		System.out.println(result);
+		
+		scan.close();
+	}
+
+	private static boolean isPalindrome(int x) {
+		
+	      StringBuilder num = new StringBuilder();
+	      
+	      num.append(x);
+
+	      int length = num.length();
+
+	      for(int i = 0; i < length/2; i++) {
+	    	  if(num.charAt(i) != num.charAt(length - i)) 
+	    		  return false;
+	      }
+	      
+	      return true;
+		
+	}
+
+}
