b60fc77e13d32f9bef0fb2b5ef2ffb2f763fd18f
Christian J. Ramos Ortega <92696797+christian12305@users.noreply.github.com>
2023-04-14 20:21:18 -0400

    Number of Pairs challenge
    Returns the amount of pairs in the given array and the remaining values in the array without pairs.

---
+++ NumberOfPairs.java
@@ -0,0 +1,42 @@
+package codenights;
+
+import java.util.*;
+
+public class NumberOfPairs {
+
+	public static void main(String[] args) {
+
+
+		int nums[] = {1,3,2,1,3,2,2};
+		
+		int result[] = numberOfPairs(nums);
+		
+		System.out.println(Arrays.toString(result));
+	}
+	
+	//
+	public static int[] numberOfPairs(int[] nums) {
+        
+        int count = 0;
+        
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        
+        for(int n : nums) {
+        	
+        	//First ocurrence
+        	if(!list.contains(n))
+        		list.add(n);
+        	else{
+        			//pair
+        			count++;
+        			list.removeFirstOccurrence(n);
+        		
+        	}
+        }
+
+        int answer[] = {count, list.size()};
+        
+        return answer;
+    }
+
+}
