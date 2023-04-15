abe52fa33a75bbf19353deba53fe868a60189641
Christian J. Ramos Ortega <92696797+christian12305@users.noreply.github.com>
2023-04-14 20:17:19 -0400

    Average Salary challenge
    This average does not take into account the smallest or the largest value.

---
+++ AverageSalary.java
@@ -0,0 +1,23 @@
+package codenights;
+
+import java.util.Arrays;
+
+public class AverageSalary {
+
+	public static void main(String[] args) {
+		int salary[] = {48000,59000,99000,13000,78000,45000,31000,17000,39000,37000,93000,77000,33000,28000,4000,54000,67000,6000,1000,11000};
+		System.out.format("%.5f", average(salary));
+	}
+    public static double average(int[] salary) {
+    	Arrays.sort(salary);
+    	
+    	double sum = 0;
+    	
+    	for(int i =1; i < salary.length-1; i++) 
+    		sum += salary[i];
+   	
+		return sum/(salary.length-2);
+        
+    }
+
+}
