7d1bee7d42d0e42ac3709e2149c76ea4dad48171
Christian Ramos <christian.ramos24@upr.edu>
2023-07-03 16:22:53 -0400

    Leetcode: 1431 Greatest Number of Candies

---
+++ greatestNumCandies.js
@@ -0,0 +1,29 @@
+/**
+ * Leetcode: 1431. Kids With the Greatest Number of Candies
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+
+    //get the max
+    //loop
+    //if candies[i] + extraCandies >= max
+    //result[i] = true
+    //else
+    //result[i] = false;
+
+    let result = [];
+
+    let max = Math.max(...candies);
+
+    for(let i = 0; i < candies.length; i++){
+        if((candies[i] + extraCandies) >= max){
+            result[i] = true;
+        }else{
+            result[i] = false;
+        }
+    }
+
+    return result;
+};
