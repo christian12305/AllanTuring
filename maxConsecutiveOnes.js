3df06595f30c8bbc5f391140af8384b8aad687ef
Christian Ramos <christian.ramos24@upr.edu>
2023-07-06 00:12:39 -0400

    Leetcode: 1004. Max Consecutive Ones III

---
+++ maxConsecutiveOnes.js
@@ -0,0 +1,45 @@
+/**
+ * Leetcode: 1004. Max Consecutive Ones III
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    //loop nums
+    //  if nums[right] is 0
+    //      decrease k
+    //  if k < 0
+    //      while first elem of window not 0
+    //          shrink window from left
+    //      k++
+    //      left++
+    //  max of max and window size
+    //
+    //return max
+    //
+    let maxCount = left = 0;
+
+    //extend window
+    for(let right = 0; right < nums.length; right++){
+        //zero flip
+        if(nums[right] === 0){
+            k--;
+        }
+
+        //adjust window if no flip available
+        if(k < 0){
+            //move until the first 0, which is what we want to remove
+            while(nums[left] !== 0){
+                left++;
+            }
+            //removes first 0
+            left++
+            k++
+        }
+
+        maxCount = Math.max(maxCount, right - left + 1);
+
+    }
+
+    return maxCount;
+};
