741371b711298070873f7d53e0d4ac02ae25129f
Christian Ramos <christian.ramos24@upr.edu>
2023-08-03 19:48:55 -0400

    Fixed a comment

--- onlineStockSpan.java
+++ onlineStockSpan.java
@@ -10,7 +10,7 @@ class StockSpanner {
 
     //in the monotonic stack we want to keep the elements that are smaller than
     //the current price, if price is bigger than top, we remove from stack until its not bigger than top
-    //store in the removed index the difference between current index and the index of the removed
+    //store in the removed index the amount of days to find a greater value
 
     private Stack<int[]> mono;
 
@@ -25,19 +25,20 @@ class StockSpanner {
 
         //if stack is not empty and the given price is greater than the top of the stack
         //remove element and add to the answer the days needed for the last element at
-        // the top to reach a greater number (because if this number is higher
+        // the top to reach a greater number 
+        // (because if this number is higher
         // than the last number at the top, then it means that for the current number
-        // to find a higher element, it must also go through those days)
+        // to find a higher element, it must also go through the days of the element before it to
+        // find the larger number)
         //
-        while(!mono.isEmpty() && mono.peek()[0] < price){
+        while(!mono.isEmpty() && mono.peek()[0] <= price){
             //since we are keeping a monotonic stack,
             //when finding a price greater than the top
-            //remove element and add to answer its ans
-
+            //remove element and add to answer its number of days
             ans += mono.pop()[1];
         }
 
-        //add current [price, the number of days needed at last point] to the stack
+        //add current [price, the number of days needed] to the stack
         mono.push(new int[]{price, ans});
 
         return ans;
